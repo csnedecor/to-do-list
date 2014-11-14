721ad8da4a468ecc2471709d9f5a44f6e891c6d9
Cori Snedecor <coriannas@yahoo.com>
2014-11-14 13:34:57 -0500

    Allow user input to build list

--- server.rb
+++ server.rb
@@ -1,13 +1,25 @@
 require 'sinatra'
+require 'pry'
 
 set :public_folder, File.dirname(__FILE__) + '/public'
 
 get '/' do
-  @tasks = ['pay bills', 'buy milk', 'learn Ruby']
+  @tasks = File.readlines('tasks')
   erb :index
 end
 
 get '/tasks/:task_name' do
   @task = params[:task_name]
   erb :show
+end
+
+post '/tasks' do
+
+  task = params['task_name']
+
+  File.open('tasks', 'a') do |file|
+    file.puts(task)
+  end
+
+  redirect '/'
 end
